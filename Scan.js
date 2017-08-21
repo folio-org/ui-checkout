e3340c6a90a9d71142b3d30034197eb7c191e08e
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-08-21 18:18:28 +0200

    Fix item field focus.

--- Scan.js
+++ Scan.js
@@ -105,7 +105,7 @@ class Scan extends React.Component {
         if (json.users.length === 0) {
           throw new SubmissionError({ patron: { identifier: `User with this ${patronIdentifier.label} does not exist`, _error: 'Scan failed' } });
         }
-        document.getElementById('barcode').focus();
+        document.getElementById('input-item-barcode').focus();
         return this.props.mutator.patrons.replace(json.users);
       });
   }
