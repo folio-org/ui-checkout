3c3a56a55a925b7dd085d9f5977934a45cb4fc98
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-08-25 17:46:38 +0200

    Remove loan policy settings. UICHKOUT-10.

--- settings/index.js
+++ settings/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import Settings from '@folio/stripes-components/lib/Settings';
 
 import ScanCheckoutSettings from './ScanCheckoutSettings';
-import LoanPolicySettings from './LoanPolicySettings';
 
 const pages = [
   {
     route: 'checkout',
     label: 'Check-out',
     component: ScanCheckoutSettings,
-  },
-  {
-    route: 'loan-policies',
-    label: 'Loan policies',
-    component: LoanPolicySettings,
   },
 ];
 
