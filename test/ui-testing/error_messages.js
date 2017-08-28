4c9965489f7dc3adf28369924c371bdee6ff6c10
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-08-28 09:52:48 +0200

    Align with reorganized helpers. Output version info. STRIPES-801.

--- test/ui-testing/error_messages.js
+++ test/ui-testing/error_messages.js
@@ -1,23 +1,22 @@
 module.exports.test = function(uiTestCtx) {
 
-  describe('Module test: checkout:error_messages', function() {
-    const { config, utils: { auth, names } } = uiTestCtx;
-    const nightmare = new Nightmare(config.nightmare);
+  const { config, helpers: { login, openApp, logout }, meta: { testVersion } } = uiTestCtx;
 
+  describe('Module test: checkout:error_messages.', function() {
+    const nightmare = new Nightmare(config.nightmare);
     this.timeout(Number(config.test_timeout));
 
-    describe('Login > Open module "Check Out" > Logout', () => {
+    describe('Open app > Trigger error messages > Logout', () => {
       before( done => {
-        auth.login(nightmare,config,done);
+        login(nightmare, config, done);
       })
-      it('should open module "Check Out"', done => {
+      after( done => {
+        logout(nightmare, config, done);
+      })
+      it('should open app and find module version tag', done => {
         nightmare
-        .wait('#clickable-checkout-module')
-        .click('#clickable-checkout-module')
-        .then(function(result) {
-          done()
-        })
-        .catch(done)
+        .use(openApp(nightmare, config, done, 'checkout', testVersion))
+        .then(result => result )
       })
       it('should show error when scanning item before patron card', done => {
         nightmare
@@ -97,9 +96,6 @@ module.exports.test = function(uiTestCtx) {
         .wait(parseInt(process.env.FOLIO_UI_DEBUG) ? parseInt(config.debug_sleep) : 555) // debugging
         .then(result => { done() })
         .catch(done)
-      })
-      it('should log out', done => {
-          auth.logout(nightmare,config,done);
       })
     })
   })
