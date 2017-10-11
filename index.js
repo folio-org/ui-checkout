6d4b86d7c45af10e1d4e5f99911617d9b469213c
Zak Burke <zburke@cornell.edu>
2017-10-11 16:30:48 -0400

    Use PropTypes, not React.PropTypes. Refs FOLIO-427

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'; 
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Scan from './Scan';
@@ -7,7 +8,7 @@ import Settings from './settings';
 class CheckOutRouting extends React.Component {
 
   static childContextTypes = {
-    history: React.PropTypes.object,
+    history: PropTypes.object,
   };
 
   static propTypes = {
