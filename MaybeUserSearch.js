77fe20ed312270bcf59f4267d535f999f2ef3510
Michal Kuklis <michal.kuklis@gmail.com>
2017-11-27 05:52:09 -0500

    Change patron lookup

--- MaybeUserSearch.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Pluggable from '@folio/stripes-components/lib/Pluggable';
-
-export default props => (
-  <Pluggable {...props} type="find-user">
-    <span />{/* nothing to render here */}
-  </Pluggable>
-);
