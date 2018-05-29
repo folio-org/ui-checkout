06261aa898359dd824edb35c44bc4b0023ba7f49
Michal Kuklis <michal.kuklis@gmail.com>
2018-05-29 16:07:29 -0400

    Remove old code responsible for the checkout processing

--- constants.js
+++ constants.js
@@ -5,22 +5,6 @@ export const errorTypes = {
   ITEM_CHECKED_OUT: 4,
 };
 
-export const intervalPeriodsMap = {
-  MINUTE: 'minutes',
-  HOUR: 'hours',
-  DAY: 'days',
-  WEEK: 'weeks',
-  MONTH: 'months',
-};
-
-export const intervalIdsMap = {
-  'Minutes': 'minutes',
-  'Hours': 'hours',
-  'Days': 'days',
-  'Weeks': 'weeks',
-  'Months': 'months',
-};
-
 export const patronIdentifierMap = {
   BARCODE: 'barcode',
   EXTERNAL: 'externalSystemId',
@@ -35,17 +19,6 @@ export const patronLabelMap = {
   USER: 'username',
 };
 
-export const renewFromMap = {
-  CURRENT_DUE_DATE: '1',
-  SYSTEM_DATE: '2',
-};
-
 export const defaultPatronIdentifier = 'BARCODE';
-
-export const loanProfileTypesMap = {
-  FIXED: 'Fixed',
-  ROLLING: 'Rolling',
-  INDEFINITE: 'Indefinite',
-};
 
 export default '';
