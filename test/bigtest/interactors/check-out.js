62bf02530d02a30a7d4004e814932afeee8b7912
oleksandr_antonenko <Oleksandr_Antonenko@epam.com>
2019-04-05 19:29:52 +0300

    UIU-879: Add tests.

--- test/bigtest/interactors/check-out.js
+++ test/bigtest/interactors/check-out.js
@@ -4,22 +4,43 @@ import {
   interactor,
   isPresent,
   text,
+  scoped,
 } from '@bigtest/interactor';
 
-@interactor class ScanItemsInteractor {
+@interactor
+class ScanItemsInteractor {
   multipieceModalPresent = isPresent('#multipiece-modal');
 }
 
-@interactor class CheckoutNoteModalInteractor {
+@interactor
+class CheckoutNoteModalInteractor {
   present = isPresent('[data-test-checkoutNoteModal-confirm-button]');
   clickConfirm = clickable('[data-test-checkoutNoteModal-confirm-button]');
+}
+
+@interactor
+class ErrorModal {
+  static defaultScope = '[data-test-error-modal]';
+
+  overrideButton = scoped('[data-test-override-button]');
+  closeButton = scoped('[data-test-close-button]');
+}
+
+@interactor
+class OverrideModal {
+  static defaultScope = '[data-test-override-modal]';
+
+  dueDatePicker = scoped('[ data-test-override-modal-due-date-picker]');
+  comment = scoped('[data-test-override-modal-comment]');
+  saveAndCloseButton = scoped('[data-test-override-modal-save-and-close]');
+  cancelButton = scoped('[data-test-override-modal-cancel]');
 }
 
 
 export default interactor(class CheckOutInteractor {
   static defaultScope = '[data-test-check-out-scan]';
-  scanItems = new ScanItemsInteractor('[data-test-scan-items]')
- checkoutNoteModal = new CheckoutNoteModalInteractor();
+  scanItems = new ScanItemsInteractor('[data-test-scan-items]');
+  checkoutNoteModal = new CheckoutNoteModalInteractor();
   patronIdentifierPresent = isPresent('#input-patron-identifier');
   patronEnterBtnPresent = isPresent('#clickable-find-patron');
   fillPatronBarcode = fillable('#input-patron-identifier');
@@ -31,4 +52,7 @@ export default interactor(class CheckOutInteractor {
 
   patronFullName = text('[data-test-check-out-patron-full-name]');
   awaitPickupModalPresent = isPresent('#awaiting-pickup-modal');
+
+  overrideModal = new OverrideModal();
+  errorModal = new ErrorModal();
 });
--- test/bigtest/network/config.js
+++ test/bigtest/network/config.js
@@ -1,3 +1,4 @@
+import { Response } from '@bigtest/mirage';
 import CQLParser from './cql';
 
 // typical mirage config export
@@ -19,6 +20,73 @@ export default function config() {
   this.get('/manualblocks', {
     manualblocks: [],
     totalRecords: 0,
+  });
+
+  this.post('circulation/check-out-by-barcode', () => {
+    return new Response(422, { 'Content-Type': 'application/json' }, {
+      'errors' : [{
+        'message' : 'Item is not loanable',
+        'parameters' : [{
+          'key' : 'itemBarcode',
+          'value' : '5860825104574'
+        }]
+      }]
+    });
+  });
+
+  this.get('/groups', {
+    'usergroups' : [{
+      'group' : 'faculty',
+      'desc' : 'Faculty Member',
+      'id' : 'cb5c73ae-092f-4c28-8a52-4c7396937e09',
+      'metadata' : {
+        'createdDate' : '2019-03-10T11:02:41.298+0000',
+        'createdByUserId' : '1582c806-9824-51c0-abff-d98432283103',
+        'updatedDate' : '2019-03-10T11:02:41.298+0000',
+        'updatedByUserId' : '1582c806-9824-51c0-abff-d98432283103'
+      }
+    }, {
+      'group' : 'staff',
+      'desc' : 'Staff Member',
+      'id' : 'e4554dc5-bd88-4076-905e-73f99ce49101',
+      'metadata' : {
+        'createdDate' : '2019-03-10T11:02:42.481+0000',
+        'createdByUserId' : '1582c806-9824-51c0-abff-d98432283103',
+        'updatedDate' : '2019-03-10T11:02:42.481+0000',
+        'updatedByUserId' : '1582c806-9824-51c0-abff-d98432283103'
+      }
+    }, {
+      'group' : 'graduate',
+      'desc' : 'Graduate Student',
+      'id' : '66a0affc-b060-466b-bc39-edbd0ae6221d',
+      'metadata' : {
+        'createdDate' : '2019-03-10T11:02:43.644+0000',
+        'createdByUserId' : '1582c806-9824-51c0-abff-d98432283103',
+        'updatedDate' : '2019-03-10T11:02:43.644+0000',
+        'updatedByUserId' : '1582c806-9824-51c0-abff-d98432283103'
+      }
+    }, {
+      'group' : 'undergrad',
+      'desc' : 'Undergraduate Student',
+      'id' : '69b07fe4-32b5-4780-83c3-e99a168f0e08',
+      'metadata' : {
+        'createdDate' : '2019-03-10T11:02:44.814+0000',
+        'createdByUserId' : '1582c806-9824-51c0-abff-d98432283103',
+        'updatedDate' : '2019-03-10T11:02:44.814+0000',
+        'updatedByUserId' : '1582c806-9824-51c0-abff-d98432283103'
+      }
+    }],
+    'totalRecords' : 4
+  });
+
+  this.get('/accounts', {
+    'accounts' : [],
+    'totalRecords' : 0,
+    'resultInfo' : {
+      'totalRecords' : 0,
+      'facets' : [],
+      'diagnostics' : []
+    }
   });
 
   // requests
