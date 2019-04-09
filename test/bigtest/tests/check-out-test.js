07f679ca005be30defbc6f6d5b5cf8ac1f4c7cfe
Matt Connolly <mjc12@cornell.edu>
2019-04-09 16:18:51 -0400

    Add tests for item list and menu

--- test/bigtest/tests/check-out-test.js
+++ test/bigtest/tests/check-out-test.js
@@ -73,6 +73,65 @@ describe('CheckOut', () => {
         .clickPatronBtn();
     });
 
+    describe('checking out a single item', () => {
+      beforeEach(async function () {
+        this.server.create('item', {
+          barcode: '123',
+          title: 'Book 1',
+        });
+        this.server.create('loan');
+
+        await checkOut
+          .fillItemBarcode('123')
+          .clickItemBtn();
+      });
+
+      it('shows a list of checked out items', () => {
+        expect(checkOut.scanItems.itemListPresent).to.be.true;
+      });
+    });
+
+    describe('using the item menu', () => {
+      beforeEach(async function () {
+        this.server.create('item', {
+          barcode: '123',
+          title: 'Book 1',
+          instanceId: 'instance1',
+          holdingsRecordId: 'holdings1'
+        });
+        this.server.create('loan');
+
+        await checkOut
+          .fillItemBarcode('123')
+          .clickItemBtn()
+          .itemMenu.clickItemMenu();
+      });
+
+      describe('choosing item details', () => {
+        beforeEach(async function () {
+          await checkOut.itemMenu.selectItemDetails();
+        });
+
+        it('redirects to item details page', function () {
+          const { search, pathname } = this.location;
+          expect(pathname + search).to.include('/inventory/view/instance1/holdings1/123');
+        });
+      });
+
+      describe('choosing loan details', () => {
+        beforeEach(async function () {
+          await checkOut.itemMenu.selectLoanDetails();
+        });
+
+        it('redirects to (user) loan details page', function () {
+          const { search, pathname } = this.location;
+          expect(pathname + search).to.include('/users/view/df7f4993-8c14-4a0f-ab63-93975ab01c76');
+          expect(search).to.include('layer=loan');
+          expect(search).to.include('loan=cf23adf0-61ba-4887-bf82-956c4aae2260');
+        });
+      });
+    });
+
     describe('checking out multipiece item', () => {
       beforeEach(async function () {
         this.server.create('item', {
