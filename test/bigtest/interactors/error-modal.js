d498f1d320286bffcb25ca93217441e1a33f6fde
Sergiy Sergiyenko <Sergiy_Sergiyenko@epam.com>
2021-01-05 23:45:32 +0200

    UICHKOUT-669: Prevent check out for intellectual item status (#573)

--- test/bigtest/interactors/error-modal.js
+++ test/bigtest/interactors/error-modal.js
@@ -1,11 +1,13 @@
 import {
   interactor,
   scoped,
+  text,
 } from '@bigtest/interactor';
 
 @interactor class ErrorModal {
   static defaultScope = '[data-test-error-modal]';
 
+  content = text('[class^="modalContent---"] p');
   overrideButton = scoped('[data-test-override-button]');
   closeButton = scoped('[data-test-close-button]');
 }
