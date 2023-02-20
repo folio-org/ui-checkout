56ec4648ae779b9428680b7ff8369393b0c685a4
nikolai <nikolai_litvinenko@epam.com>
2023-02-20 16:12:47 +0400

    UICHKOUT-823: UI tests replacement with RTL/Jest for src/index.js

---
+++ src/index.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+
+import { stripes } from '../package';
+
+import '../test/jest/__mock__';
+
+import CheckOutRouting from './index';
+
+const { route } = stripes;
+const testIds = {
+  checkOutComponent: 'checkOutComponent',
+  noMatch: 'noMatch',
+};
+
+jest.mock('./CheckOut', () => () => <div data-testid={testIds.checkOutComponent}>CheckOut</div>);
+
+describe('UI CheckOut', () => {
+  const renderCheckOut = () => {
+    const component = (
+      <Router>
+        <CheckOutRouting
+          stripes={{
+            connect: (item) => item,
+          }}
+          match={{
+            path: route,
+            url: route,
+            isExact: true,
+            params: {},
+          }}
+          location={{
+            pathname: route,
+            search: '',
+            hash: '',
+          }}
+        />
+      </Router>
+    );
+
+    return render(component);
+  };
+
+  it('should render', () => {
+    renderCheckOut();
+
+    expect(screen.getByTestId(testIds.noMatch)).toBeInTheDocument();
+  });
+
+  it('should render on component route', () => {
+    window.history.pushState({}, '', route);
+
+    renderCheckOut();
+
+    expect(screen.getByTestId(testIds.checkOutComponent)).toBeInTheDocument();
+  });
+});
